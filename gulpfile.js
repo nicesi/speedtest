b50efb774199c8bf93e25a4c686d5560e6ba556b
Ramazan Sancar <ramazansancar4545@gmail.com>
2024-02-03 22:00:07 +0300

    update: flags updated. update: index.html new template updated. create: test space created.

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+// https://medium.com/devux/minifying-your-css-js-html-files-using-gulp-2113d7fcbd16
+'use strict';
+
+const gulp = require('gulp');
+const del = require('del');
+const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+const csso = require('gulp-csso');
+const htmlmin = require('gulp-htmlmin');
+const uglify = require('gulp-uglify-es').default;
+
+
+gulp.task('copyall', () => {
+    return gulp.src('./**/*').pipe(gulp.dest('./dist'));
+});
+
+gulp.task('styles', () => {
+    return gulp.src('./**/*.css')
+        .pipe(autoprefixer())
+        .pipe(csso())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('scripts', () => {
+    return gulp.src('./**/*.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./dist'))
+});
+
+gulp.task('pages', () => {
+    return gulp.src(['./**/*.html'])
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeComments: true
+        }))
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('clean', () => del(['dist']));
+
+gulp.task('default', gulp.series('clean', 'copyall', gulp.parallel('styles', 'scripts', 'pages')));
